69fe0b93afe00638e68a222317e7c2b73007f852
Pedro E Garcia <pg548169@gmail.com>
2020-01-20 16:31:49 -0600

    Finalización del modulo

---
+++ src/java/model/IUsuarioModel.java
@@ -0,0 +1,26 @@
+/* 
+ * Autor:  Pedro Eliezer García Ramírez
+ * Email: pedro.e.garcia.ramirez@gmail.com
+ * Creación: 26/11/2019
+ *
+ */
+package model;
+
+import entity.Usuario;
+import java.util.List;
+
+
+public interface IUsuarioModel {
+
+    public void CrearUsuario(Usuario usuario);
+
+    public void ActualizarUsuario(Usuario usuario);
+
+    public void EliminarRegistro(Usuario usuario);
+
+    public Usuario ObtenerRegistro(long idusuario);
+
+    public List<Usuario> ObtenerRegistros();
+    
+    
+}
---
+++ src/java/model/UsuarioModel.java
@@ -0,0 +1,103 @@
+/* 
+ * Autor:  Pedro Eliezer García Ramírez
+ * Email: pedro.e.garcia.ramirez@gmail.com
+ * Creación: 26/11/2019
+ *
+ */
+package model;
+
+import entity.Usuario;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class UsuarioModel implements IUsuarioModel {
+
+    private SessionFactory sessionFactory;
+    private Session session;
+
+    @Override
+    public void CrearUsuario(Usuario usuario) {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.save(usuario);
+            session.getTransaction().commit();
+            session.close();
+            sessionFactory.close();
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void ActualizarUsuario(Usuario usuario) {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.update(usuario);
+            session.getTransaction().commit();
+            session.close();
+            sessionFactory.close();
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void EliminarRegistro(Usuario usuario) {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.delete(usuario);
+            session.getTransaction().commit();
+            session.close();
+            sessionFactory.close();
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public Usuario ObtenerRegistro(long idusuario) {
+
+        Usuario usuario = null;
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+            session = sessionFactory.openSession();
+            usuario = (Usuario) session.get(Usuario.class, idusuario);
+            session.close();
+            sessionFactory.close();
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+        }
+        return usuario;
+    }
+
+    @Override
+    public List<Usuario> ObtenerRegistros() {
+        ArrayList<Usuario> lista = null;
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+            session = sessionFactory.openSession();
+            lista = (ArrayList<Usuario>) session.createQuery("FROM Usuario").list();
+            for (Usuario u : lista) {
+
+            }
+            session.close();
+            sessionFactory.close();
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+        }
+        return lista;
+    }
+    //Usuario us = new Usuario(0, nombre, tipo, contrasena, usuario);
+
+}
